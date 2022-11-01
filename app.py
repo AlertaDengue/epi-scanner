89e7a08dc176080d9f9b69c0f429d8f8cb28fd0b
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-11-01 18:25:49 -0300

    Added state selection dropdown

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import time
 
+import pandas as pd
 import psutil
-from h2o_wave import ui, data, Q, app, main
+from h2o_wave import ui, data, Q, app, on, main
 from loguru import logger
+
+DATA_TABLE = None
+UF = None
 
 
 @app('/', mode='unicast')
@@ -19,10 +23,12 @@ async def serve(q: Q):
     add_sidebar(q)
     q.page['footer'] = ui.footer_card(box='footer', caption='(c) 2022 Infodengue. All rights reserved.')
     while True:
-        try:
-            await update_stats(q)
-        except Exception as e:
-            logger.debug(f"Exception while updating: {e}")
+        # logger.info(q.args.state)
+        logger.info(q.args.state)
+        await load_table(q)
+
+        await update_stats(q)
+        # await q.page.save()
 
 
 def create_layout(q):
@@ -52,8 +58,28 @@ def create_layout(q):
         )
     ])
 
+
+
+
+async def load_table(q: Q):
+    if q.args.state is not None:
+        global DATA_TABLE
+        global UF
+        uf = q.args.state
+        if (DATA_TABLE is None) or uf != UF:
+            DATA_TABLE = pd.read_parquet(f"{uf}_dengue.parquet")
+            choices = [ui.choice(str(gc), str(gc)) for gc in set(DATA_TABLE.municipio_geocodigo)]
+            q.page['form'].items.append(
+                ui.dropdown(name='city', label='Select city', required=True,
+                            choices=choices, trigger=True)
+                )
+            UF = uf
+    await q.page.save()
+
+
 async def update_analysis(q):
     pass
+
 
 async def update_stats(q: Q):
     cpu_usage = psutil.cpu_percent(interval=1)
@@ -66,14 +92,17 @@ async def update_stats(q: Q):
 
     await q.page.save()
 
+
 def add_sidebar(q):
     state_choices = [
-        ui.choice('SC', 'Santa Catarina')
+        ui.choice('SC', 'Santa Catarina'),
+        ui.choice('RS', 'Rio Grande do Sul')
     ]
-    q.page['form'] = ui.form_card(box='sidebar', items = [
-        ui.dropdown(name='State', label='Select state', value='SC', required=True,
+    q.page['form'] = ui.form_card(box='sidebar', items=[
+        ui.dropdown(name='state', label='Select state', value='RS', required=True,
                     choices=state_choices, trigger=True),
     ])
+
 
 def add_stats_cards(q):
     q.page['cpu'] = ui.small_series_stat_card(
