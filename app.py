09a41a5b9bfd22ef965bd60f5ba10ce3cbbed577
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-11-02 18:15:26 -0300

    Added choropleth map of weeks of treansmission

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import psutil
 from h2o_wave import ui, data, Q, app, main, copy_expando
 from loguru import logger
-from viz import load_map, plot_state_map
+from viz import load_map, plot_state_map, t_weeks, update_state_map
 
 DATA_TABLE = None
 
@@ -19,7 +19,7 @@ async def initialize_app(q: Q):
         box=ui.box('header'),
         title='Real-time Epidemic Scanner',
         subtitle='Real-time epidemiology',
-        # image='/assets/info-dengue-logo.png',
+        # image='assets/info-dengue-logo.png',
         icon='health'
     )
     await q.page.save()
@@ -27,14 +27,14 @@ async def initialize_app(q: Q):
     # add_stats_cards(q)
     await q.page.save()
     UF = q.client.uf = 'SC'
-
-    fig = await plot_state_map(q, q.client.brmap, UF)
+    await update_state_map(q)
+    fig = await plot_state_map(q, q.client.statemap, UF)
     q.page['plot'] = ui.markdown_card(box='content', title=f'Map of {UF}', content=f'![plot]({fig})')
     add_sidebar(q)
     q.page['footer'] = ui.footer_card(box='footer', caption='(c) 2022 Infodengue. All rights reserved.')
 
 
-@app('/', mode='multicast')
+@app('/scanner', mode='multicast')
 async def serve(q: Q):
     copy_expando(q.args, q.client)
     create_layout(q)
@@ -43,19 +43,26 @@ async def serve(q: Q):
     q.client.cities = {}
     q.client.loaded = ''
     q.client.uf = 'SC'
+    q.client.weeks = False
     while True:
         if q.args.state:
             await update_UF(q)
         if q.args.city:
             await update_city(q)
-        # logger.info(f'UF: {UF}, state: {q.args.state}, city:{q.client.city}')
+        # logger.info(f'UF: {q.args}, state: {q.args.state}, city:{q.client.city}')
         await load_table(q)
-        # await update_stats(q)
+        if (not q.client.weeks) and (q.client.data_table is not None):
+            await t_weeks(q)
+            fig = await plot_state_map(q, q.client.weeks_map, q.client.uf, column='transmissao')
+            q.page['plotweeks'] = ui.markdown_card(box='content',
+                                                   title=f'Number of weeks of $R_t>1$ in the last 10 years',
+                                                   content=f'![plot]({fig})')
+
         await q.page.save()
 
 
 async def update_UF(q: Q):
-    logger.info(f'UF: {q.client.uf}, state: {q.args.state}, city:{q.client.city}')
+    logger.info(f'UF: {q.client.uf}, state: {q.args.state}, city:{q.args.city}')
     q.client.uf = q.args.state
 
 
@@ -81,7 +88,7 @@ def create_layout(q):
                                     ),
                             ui.zone('content',
                                     size='75%',
-                                    # direction=ui.ZoneDirection.ROW,
+                                    direction=ui.ZoneDirection.ROW,
                                     # align='end',
                                     # justify='around',
                                     ),
@@ -102,6 +109,7 @@ async def load_table(q: Q):
     if DATA_TABLE is None and os.path.exists(f"data/{UF}_dengue.parquet"):
         logger.info("loading data...")
         DATA_TABLE = pd.read_parquet(f"data/{UF}_dengue.parquet")
+        q.client.data_table = DATA_TABLE
         q.client.loaded = True
         for gc in DATA_TABLE.municipio_geocodigo.unique():
             q.client.cities[gc] = q.client.brmap[q.client.brmap.code_muni.astype(int) == int(gc)].name_muni.values[0]
@@ -118,18 +126,6 @@ async def update_analysis(q):
     pass
 
 
-async def update_stats(q: Q):
-    cpu_usage = psutil.cpu_percent(interval=1)
-    q.page['cpu'].data.usage = cpu_usage
-    q.page['cpu'].plot_data[-1] = [time.ctime(), cpu_usage]
-
-    mem_usage = psutil.virtual_memory().percent
-    q.page['memory'].data.usage = mem_usage
-    q.page['memory'].plot_data[-1] = [time.ctime(), mem_usage]
-
-    await q.page.save()
-
-
 def add_sidebar(q):
     state_choices = [
         ui.choice('SC', 'Santa Catarina'),
@@ -137,33 +133,7 @@ def add_sidebar(q):
     ]
     q.page['form'] = ui.form_card(box='sidebar', items=[
         ui.dropdown(name='state', label='Select state', value='RS', required=True,
-                    choices=state_choices, trigger=True),
+                    choices=state_choices, trigger=False),
         ui.dropdown(name='city', label='Select city', required=True,
-                    choices=[], trigger=True, visible=False)
+                    choices=[], trigger=False, visible=False)
     ])
-
-
-def add_stats_cards(q):
-    q.page['cpu'] = ui.small_series_stat_card(
-        box='content',
-        title='CPU',
-        value='={{usage}}%',
-        data=dict(usage=0.0),
-        plot_data=data('tick usage', -15),
-        plot_category='tick',
-        plot_value='usage',
-        plot_zero_value=0,
-        plot_color='$red',
-    )
-
-    q.page['memory'] = ui.small_series_stat_card(
-        box='content',
-        title='Memory',
-        value='={{usage}}%',
-        data=dict(usage=0.0),
-        plot_data=data('tick usage', -15),
-        plot_category='tick',
-        plot_value='usage',
-        plot_zero_value=0,
-        plot_color='$blue',
-    )
--- viz.py
+++ viz.py
@@ -11,18 +11,26 @@ async def load_map(q: Q):
     q.client.brmap = brmap
 
 
-async def plot_state_map(q, brmap: gpd.GeoDataFrame, uf: str = 'SC', column=None):
-    statemap = brmap[brmap.abbrev_state == uf]
-    ax = statemap.plot(column=column, legend=True,
-                       scheme='natural_breaks',
-                       legend_kwds={'loc': 'lower left'})
+async def update_state_map(q: Q):
+    statemap = q.client.brmap[q.client.brmap.abbrev_state == q.client.uf]
+    q.client.statemap = statemap
+
+
+async def t_weeks(q: Q):
+    weeks = q.client.data_table.groupby(by='municipio_geocodigo').sum(numeric_only=True)[['transmissao']]
+    wmap = q.client.statemap.merge(weeks, left_on='code_muni', right_index=True)
+    q.client.weeks_map = wmap
+
+
+async def plot_state_map(q, themap: gpd.GeoDataFrame, uf: str = 'SC', column=None):
+    ax = themap.plot(column=column, legend=True,
+                     scheme='natural_breaks',
+                     legend_kwds={'loc': 'lower left'})
     ax.set_axis_off()
     image_filename = f'{str(uuid.uuid4())}.png'
     plt.savefig(image_filename)
-
     # Upload
     image_path, = await q.site.upload([image_filename])
-
     # Clean up
     os.remove(image_filename)
     return image_path
