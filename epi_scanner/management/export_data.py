0a89bc4e0a8842c5da5498eb850a09063e2fc202
Sandro Loch <es.loch@gmail.com>
2023-04-26 12:34:18 -0300

    feat(modules): Add support for exporting data for multiple diseases

--- epi_scanner/management/export_data.py
+++ epi_scanner/management/export_data.py
@@ -1,5 +1,6 @@
+import argparse
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 import pandas as pd
 from epi_scanner.management.fetch_data import data_to_parquet
@@ -7,7 +8,9 @@ from epi_scanner.model.scanner import EpiScanner
 from epi_scanner.settings import EPISCANNER_DATA_DIR, STATES
 
 
-def export_data_to_dir(state: str, output_dir: Optional[str] = None) -> None:
+def export_data_to_dir(
+    state: str, diseases: List[str], output_dir: Optional[str] = None
+) -> None:
     """
     Export data for a single state to Parquet and CSV files.
 
@@ -15,6 +18,8 @@ def export_data_to_dir(state: str, output_dir: Optional[str] = None) -> None:
     ----------
     state : str
         The abbreviation of the state to export.
+    diseases : list of str
+        The diseases to export data for.
     output_dir : str, optional
         The directory where the output files will be saved.
         If not provided, the default directory set in
@@ -37,24 +42,62 @@ def export_data_to_dir(state: str, output_dir: Optional[str] = None) -> None:
                 f"Error: {output_dir} directory does not exist."
             )
 
-        parquet_file = output_dir / f"{state}_dengue.parquet"
+        if state not in STATES:
+            raise ValueError(
+                f"""Error: Invalid state abbreviation {state}.
+                    Valid states are {STATES}.
+                """
+            )
 
-        data_to_parquet(state, output_dir=output_dir)
+        for disease in diseases:
+            parquet_file = output_dir / f"{state}_{disease}.parquet"
 
-        if parquet_file.exists():
+            if not parquet_file.exists():
+                # fetch data and save it to Parquet file
+                data_to_parquet(state, output_dir=output_dir, disease=disease)
+
+            if not parquet_file.exists():
+                raise FileNotFoundError(
+                    f"Error: Failed to save data to {parquet_file}."
+                )
+
             df = pd.read_parquet(parquet_file)
+            if df.empty:
+                raise pd.errors.EmptyDataError(
+                    f"Error: {parquet_file} is empty."
+                )
+
             SE = int(df.SE.max()) % 100
             scanner = EpiScanner(last_week=SE, data=df)
-            csv_file = output_dir / f"curves_{state}.csv.gz"
+            csv_file = output_dir / f"curves_{state}_{disease}.csv.gz"
             for gc in df.municipio_geocodigo.unique():
                 scanner.scan(gc)
             scanner.to_csv(csv_file)
 
-    except (FileNotFoundError, pd.errors.EmptyDataError) as e:
+    except (FileNotFoundError, pd.errors.EmptyDataError, ValueError) as e:
         print(e)
 
 
 if __name__ == "__main__":
-    output_dir = None
-    for state in list(STATES.keys()):
-        export_data_to_dir(state, output_dir=output_dir)
+    parser = argparse.ArgumentParser(
+        description="Export data for a single state to Parquet and CSV files."
+    )
+    parser.add_argument(
+        "state", type=str, help="The abbreviation of the state to export."
+    )
+    parser.add_argument(
+        "diseases",
+        nargs="+",
+        type=str,
+        help="The diseases to export data for.",
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=str,
+        default=None,
+        help="The directory where the output files will be saved.",
+    )
+
+    args = parser.parse_args()
+
+    export_data_to_dir(args.state, args.diseases, output_dir=args.output_dir)
